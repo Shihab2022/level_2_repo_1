e1c0c4f5a320b39de6bce332e5b72181432d3b6a
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-11-05 12:34:20 +0600

    2-5: Function with generics

--- milstone 1/module2/2.4.ts
+++ milstone 1/module2/2.4.ts
@@ -3,53 +3,54 @@
 
     // interface ----> generic
 
-    interface Developer<T,X=null>{ // here X=null because this type is optional that's way we send the optional value 
-        name:string;
-        computer:{
-            brand:string,
-            model:string,
-            releaseYear:number
+    interface Developer<T, X = null> { // here X=null because this type is optional that's way we send the optional value 
+        name: string;
+        computer: {
+            brand: string,
+            model: string,
+            releaseYear: number
         }
-        smartWatch:T;
-        bike?:X;
+        smartWatch: T;
+        bike?: X;
     }
 
-    type Poor={brand:string,model:string,display:string}
-    const poorDeveloper:Developer<{brand:string,model:string,display:string}>={ /// we can use this like Developer<Poor>
-        name:'poor',
-        computer:{
-            brand:'asus',
-            model:'s533wa',
-            releaseYear:2020
+    type Poor = { brand: string, model: string, display: string }
+    const poorDeveloper: Developer<{ brand: string, model: string, display: string }> = { /// we can use this like Developer<Poor>
+        name: 'poor',
+        computer: {
+            brand: 'asus',
+            model: 's533wa',
+            releaseYear: 2020
         },
-        smartWatch:{
-brand:'Emilab',
-model:'kw33333',
-display:'led'
-        }    }
-
-
-        interface Rich{brand:string,model:string,heartTrack:boolean,sleepTrack:boolean}
-        interface Bike{brand:string,model:string}
-        const richDeveloper:Developer<{brand:string,model:string,heartTrack:boolean,sleepTrack:boolean},{brand:string,model:string}>={ /// we can use this like Developer<Rich,Bike>
-            name:'poor',
-            computer:{
-                brand:'asus',
-                model:'s533wa',
-                releaseYear:2020
-            },
-            smartWatch:{
-    brand:'Emilab',
-    model:'kw33333',
-heartTrack:true,
-sleepTrack:true,
-            },
-bike:{
-brand:'yamaha',
-model:'fz'
-}
-
+        smartWatch: {
+            brand: 'Emilab',
+            model: 'kw33333',
+            display: 'led'
         }
+    }
+
+
+    interface Rich { brand: string, model: string, heartTrack: boolean, sleepTrack: boolean }
+    interface Bike { brand: string, model: string }
+    const richDeveloper: Developer<{ brand: string, model: string, heartTrack: boolean, sleepTrack: boolean }, { brand: string, model: string }> = { /// we can use this like Developer<Rich,Bike>
+        name: 'poor',
+        computer: {
+            brand: 'asus',
+            model: 's533wa',
+            releaseYear: 2020
+        },
+        smartWatch: {
+            brand: 'Emilab',
+            model: 'kw33333',
+            heartTrack: true,
+            sleepTrack: true,
+        },
+        bike: {
+            brand: 'yamaha',
+            model: 'fz'
+        }
+
+    }
 
 
 
