4a67e9d1dfebb7354598354608d7a13ec0e07658
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-11-17 10:34:36 +0600

    complete

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello developer!');
+//parsers
+app.use(express_1.default.json()); // when we send a json type dat then we use this
+app.use(express_1.default.text()); // when we send raw data then we use this
+const logger = (req, res, next) => {
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.hostname);
+    next();
+};
+app.get('/', logger, (req, res) => {
+    // console.log(  req.query)
+    res.send('Hello world');
+});
+app.post('/', logger, (req, res) => {
+    console.log(req.body);
+    res.send('Hello world');
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 // const express = require('express')
-import express from 'express'
+import express, { NextFunction, Request ,Response} from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//parsers
+app.use(express.json())  // when we send a json type dat then we use this
+
+app.use(express.text())// when we send raw data then we use this
+
+const logger=(req:Request,res:Response,next:NextFunction)=>{
+console.log(req.url)
+console.log(req.method)
+console.log(req.hostname)
+next()
+}
+
+app.get('/',logger, (req:Request, res:Response) => {
+// console.log(  req.query)
+  res.send('Hello world')
+})
+app.post('/',logger, (req:Request, res:Response) => {
+  console.log(req.body)
+  res.send('Hello world')
 })
 
 export default app
